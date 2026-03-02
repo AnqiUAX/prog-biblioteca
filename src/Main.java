86af64f8b45015b65131cb992e14ae102435ee9c
Anqi <anqiwang135@gmail.com>
2026-03-02 12:48:16 +0100

    feet(Main):metodo creado

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main{
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    public static void main(String[] args){
+
+        Libro libro1 = new Libro(1, "el quojote", "Migel de Cervantes",678);
+
+        Libro libro2 = new Libro(2,"el gato con botas","Charles perrault",100);
+
+        System.out.println(biblioteca.addlibro(libro1));
+        System.out.println(biblioteca.addlibro(libro2));
+
+        System.out.println(biblioteca.obtainCatalogo());
+    }
 }
