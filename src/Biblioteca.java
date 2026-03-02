cd834f79c350a3cea8cd2df7ba10090cd6eea452
Anqi <anqiwang135@gmail.com>
2026-03-02 12:00:07 +0100

    feet(biblioteca):Creada la clase biblioteca con constructor y el metodo findlibro

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Biblioteca {
+
+    private Libro[] catalogo;
+    private final int CANT_MAX_LIBRROS;
+
+    public Biblioteca(int cantidad_maxima_libros) {
+        CANT_MAX_LIBRROS = cantidad_maxima_libros;
+        catalogo = new Libro[CANT_MAX_LIBRROS];
+    }
+
+    public Libro findlibro(int idLibro){
+        boolean seguirBuscando = true;
+        Libro libroEncontrado = null;
+
+        for(int i = 0; i < CANT_MAX_LIBRROS && seguirBuscando; i++){
+            if(catalogo[i] != null && catalogo[i].getId() == idLibro){
+                seguirBuscando = false;
+                libroEncontrado = catalogo[i];
+
+            }
+        }
+        return libroEncontrado;
+    }
+
 }
